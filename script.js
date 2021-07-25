6210fbc922570bbdc35a8feeadd056c98c6e8b03
Heart <supalit1@gmail.com>
2021-07-25 22:24:13 +0700

    v.2

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
 
-var todolist=[]
-var donelist=[]
-if (localStorage.getItem('todo') === null) {
-    localStorage.setItem('todo', JSON.stringify([]));
-}
-var t= []
- t = JSON.parse(localStorage.getItem('todo'))
-
-if (localStorage.getItem('done') === null) {
-    localStorage.setItem('done', JSON.stringify([]));
-}
-var d=[]
- d = JSON.parse(localStorage.getItem('done'))
-t.forEach(t => {
-    todolist.push(t)
-    addtodo(t)
-    localStorage.todo = JSON.stringify(todolist)
-});
-
-d.forEach(d => {
-    donelist.push(d)
-    adddone(d)
-    localStorage.done = JSON.stringify(donelist)
-});
-
 let currentInput = ''
 const input = document.getElementById('input')
 input.addEventListener('input', ev => {
@@ -32,12 +7,12 @@ input.addEventListener('input', ev => {
 })
 
 const maindiv = document.createElement('div')
-maindiv.classList='flex flex-col-reverse'
+maindiv.classList='flex flex-col-reverse '
 const donediv = document.createElement('div')
 donediv.classList='flex flex-col-reverse'
 const addtask =(() =>{
     const taskdiv = document.createElement('div')
-    taskdiv.classList='flex space-x-20'
+    taskdiv.classList='flex space-x-40 task'
     const p = document.createElement('p')
     p.innerHTML = currentInput
     p.setAttribute("class","font")
@@ -60,23 +35,23 @@ const addtask =(() =>{
     del.style.visibility = "hidden"
     done.addEventListener('click', () => {
         donediv.append(taskdiv)
-        taskdiv.setAttribute("class", "doned")
+        taskdiv.setAttribute("class", "doned task")
         taskdiv.removeChild(taskdiv.childNodes[1])
-        localStorage.todo = JSON.stringify(todolist)
-        localStorage.done = JSON.stringify(donelist)
+
+       
     })
     del.addEventListener('click', () => {
         taskdiv.style.display = "none";
-        localStorage.todo = JSON.stringify(todolist)
-        localStorage.done = JSON.stringify(donelist)
+        
     })
     taskdiv.append(p, buttondiv)
     maindiv.append(taskdiv)
     buttondiv.append(done, del)
     document.body.append(maindiv, donediv)
     input.value = ''
-    localStorage.todo = JSON.stringify(todolist)
-    localStorage.done = JSON.stringify(donelist)
+   
+    
+
 
 
 
@@ -86,8 +61,6 @@ input.addEventListener('keydown', (ev) => {
     if(ev.key === 'Enter'){
         if (input.value) {
             addtask()
-            localStorage.todo = JSON.stringify(todolist)
-            localStorage.done = JSON.stringify(donelist)
         } else if (input.value === '') {
             alert("Plese filled the box!!!")
         }
@@ -98,8 +71,6 @@ input.addEventListener('keydown', (ev) => {
 const addlist = () => {
     if (input.value) {
         addtask()
-        localStorage.todo = JSON.stringify(todolist)
-        localStorage.done = JSON.stringify(donelist)
     } else if (input.value === '') {
         alert("Plese filled the box!!!")
     }
